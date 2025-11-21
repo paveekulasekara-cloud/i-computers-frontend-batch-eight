b358f141108e0e4c2737c8e189b64c81adc256be
paveekulasekara-cloud <paveekulasekara@gmail.com>
2025-11-21 17:24:42 +0530

    Day 10 ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header'
+import ProductCard from './components/productCard'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header/>
+
+      <ProductCard name="Laptop" price="100,000.00" image="https://picsum.photos/id/0/200/300"/>
+
+      <ProductCard name="Phone" price="90,000.00" image="https://picsum.photos/id/3/200/300"/>
+
+      <ProductCard name="Watch" price="5,000.00" image="https://picsum.photos/id/4/200/300"/>
     </>
   )
 }
